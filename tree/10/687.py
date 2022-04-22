33755a662232fbf6ecd946fbebdf4dc49737131b
Keith Wang <kwang@sama.com>
2022-04-21 21:37:04 -0400

    tree 687

--- tree/10/687.py
+++ tree/10/687.py
@@ -7,7 +7,8 @@
 class Solution:
     def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
         self.ans = 0
-        self._univaluePath(root)
+        # self._univaluePath(root)
+        self.getPath(root)
         return self.ans
     
     def _univaluePath(self, root):
@@ -16,5 +17,15 @@ class Solution:
         r = self._univaluePath(root.right) if root.right else -1
         pl = l + 1 if l >= 0 and root.val == root.left.val else 0
         pr = r + 1 if r >= 0 and root.val == root.right.val else 0
+        self.ans = max(self.ans, pl + pr)
+        return max(pl, pr)
+
+    def getPath(self, root):
+        if not root: return 0
+        left = self.getPath(root.left)
+        right = self.getPath(root.right)
+        pl, pr = 0, 0
+        if root.left and root.left.val == root.val: pl = left + 1
+        if root.right and root.right.val == root.val: pr = right + 1
         self.ans = max(self.ans, pl + pr)
         return max(pl, pr)

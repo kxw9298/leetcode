93914bcd01347c1ad7cbe7a7d35ce7a6367fd146
Keith Wang <kwang@sama.com>
2022-04-26 22:32:08 -0400

    list 206

---
+++ list/206.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        while head:
+            nodenext = head.next
+            head.next = dummy.next
+            dummy.next = head
+            head = nodenext
+        return dummy.next

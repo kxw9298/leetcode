68c713ea440ca5a2239b0634844c42c4a578b729
Keith Wang <kwang@sama.com>
2022-04-28 21:59:12 -0400

    list 147

---
+++ list/147.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next: return head
+        root = ListNode(0)
+        root.next = head
+        while head.next:
+            if head.val <= head.next.val:
+                head = head.next
+            else:
+                temp = head.next
+                q = root
+                head.next = head.next.next
+                while q.next and q.next.val < temp.val: # 找到第一个大于该节点的前一节点
+                    q = q.next
+                temp.next = q.next # 将 temp 节点插入
+                q.next = temp
+        return root.next

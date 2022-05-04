9fe68c0e2e59f3e970bf35b7c4fe949aff2e3fa2
Keith Wang <kwang@sama.com>
2022-05-03 21:16:34 -0400

    bst 530

---
+++ bst/530.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        self.res = float('inf')
+        self.prev = None
+        self.inOrder(root)
+        return self.res
+    
+    def inOrder(self, root):
+        if not root: return
+        self.inOrder(root.left)
+        if self.prev:
+            self.res = min(self.res, root.val - self.prev.val)
+        self.prev = root
+        self.inOrder(root.right)

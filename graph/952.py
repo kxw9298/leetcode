bc4e4eaee6e31482d1fe98d7b670d4d45ad99733
Keith Wang <keith1.wang@icloud.com>
2022-05-16 16:54:57 -0400

    graph 952

--- graph/952.py
+++ graph/952.py
@@ -2,28 +2,28 @@ class Solution:
     def largestComponentSize(self, nums: List[int]) -> int:
         mn = max(nums)
         N = len(nums)
-        m = list(range(mn+1))
+        self.p = list(range(mn+1))
         
         for n in nums:
             for k in range(2, int(math.sqrt(n))+1):
                 if n % k == 0:
-                    self.u(m, n, k)
-                    self.u(m, n, n // k)
+                    self.u(n, k)
+                    self.u(n, n // k)
         count = collections.defaultdict(int)
         for n in nums:
-            count[self.f(m,n)] += 1
+            count[self.f(n)] += 1
         return max(count.values())
     
-    def f(self, m, n):
-        while m[n] != n:
-            m[n] = m[m[n]]
-            n = m[n]
+    def f(self,n):
+        while self.p[n] != n:
+            self.p[n] = self.p[self.p[n]]
+            n = self.p[n]
         return n
         
-    def u(self, m, a, b):
-        if m[a] == m[b]: return
-        pa = self.f(m, a)
-        pb = self.f(m, b)
-        m[pa] = pb
+    def u(self, a, b):
+        if self.p[a] == self.p[b]: return
+        pa = self.f(a)
+        pb = self.f(b)
+        self.p[pa] = pb
 
 # https://blog.csdn.net/fuxuemingzhu/article/details/85015411
